63537ca574d743f1dd3ba0572b8c88a5f0bd48cd
martiN <martynosa@hotmail.com>
2022-07-30 21:34:11 +0300

    added age and gender

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const videoStream = document.getElementById('video-stream');
-console.log(faceapi.nets);
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+  faceapi.nets.ageGenderNet.loadFromUri('/models'),
 ]).then(startVideoStream);
 
 function startVideoStream() {
@@ -30,11 +30,21 @@ videoStream.addEventListener('play', () => {
     const detections = await faceapi
       .detectAllFaces(videoStream, new faceapi.TinyFaceDetectorOptions())
       .withFaceLandmarks()
-      .withFaceExpressions();
+      .withFaceExpressions()
+      .withAgeAndGender();
 
-    const resizedDetection = faceapi.resizeResults(detections, displaySize);
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetection);
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetection);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+
+    resizedDetections.forEach((detection) => {
+      const box = detection.detection.box;
+      const drawBox = new faceapi.draw.DrawBox(box, {
+        label: `${Math.round(detection.age)} years old, ${detection.gender}
+        `,
+      });
+      drawBox.draw(canvas);
+    });
   }, 100);
 });
